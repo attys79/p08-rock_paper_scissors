6162306b79d2917a1e091c10c4469b9da86d685f
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-09 17:18:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+let handshape = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . # . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(1000)
+    handshape = randint(0, 2)
+    if (handshape == 0) {
+        basic.showString("R")
+    } else if (handshape == 1) {
+        basic.showString("P")
+    } else {
+        basic.showString("S")
+    }
+})
 basic.forever(function () {
 	
 })
